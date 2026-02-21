87b727e6abe9ddbad334bd58950a7d9b3dcc663e
Michael Neumann <menumatic@proton.me>
2026-02-21 08:40:06 -0800

    Add ROM library to script

--- powershell/backup_script.ps1
+++ powershell/backup_script.ps1
@@ -14,6 +14,10 @@ $ZipPath = "G:\Backups\backup_$Date"
 echo "Backing up C:\Users\mneumatic"
 robocopy $Source $Destination /E /XC /XD .* AppData Downloads "Application Data" Cookies "Local Settings" "My Documents" "My Pictures" "My Videos" "My Music" NetHood PrintHood Recent SendTo "Start Menu" Templates /XA:H /XN /XO /R:1 /W:1
 
+# Backup for ROMs directory. Got to keep those oldies.
+
+robocopy "F:\ROMLibrary" $Destination\ROMLibrary /E /XC
+
 # Compress & delete destination folder after compression
 
 & $SevenZip a -t7z -mx=9 $ZipPath "$Destination\*" 
